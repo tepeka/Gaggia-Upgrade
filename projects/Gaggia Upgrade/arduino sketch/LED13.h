7aa594f29d14dbd3ddb7808da3b3e20fde2b9606
admin <admin@26dd76bd-a928-9f49-9f85-a93842d1d30d>
2015-04-27 21:39:18 +0000

    first sketch draft

---
+++ projects/Gaggia Upgrade/arduino sketch/LED13.h
@@ -0,0 +1,21 @@
+#ifndef LED13_H
+#define LED13_H
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+class LED13 {
+  public:
+    LED13();
+    ~LED13();
+    void on();
+    void off();
+    void blink(int time);
+};
+
+#endif
+
---
+++ projects/Gaggia Upgrade/arduino sketch/gaggia.ino
@@ -0,0 +1,39 @@
+#include "LED13.h"
+
+const int LED_PWM_PIN = 13;
+const int RELAY_PWM_PIN = 12;
+const int RTD_PWM_PIN = 9;
+
+const int LED_HANDLING_INTERVAL = 20;
+const int TEMP_READ_INTERVAL = 100;
+const int PID_CALC_INTERVAL = 1;
+
+unsigned long lastLedHandling = 0;
+unsigned long lastTempRead = 0;
+unsigned long lastPidCalc = 0;
+
+void setup() {
+  Serial.begin(115200);
+  ledInit(LED_PWM_PIN);
+  rtdInit(RTD_PWM_PIN);
+  doInit();
+}
+
+void loop() {
+  unsigned long now = millis();
+  // -- execute led pulse
+  if ((now - lastLedHandling) / LED_HANDLING_INTERVAL >= 1) {
+    doLedHandling();
+    lastLedHandling = millis();
+  }
+  // -- execute read temp
+  if ((now - lastTempRead) / TEMP_READ_INTERVAL >= 1) {
+    doReadTemp();
+    lastTempRead = millis();
+  }
+  // -- execute pid calc
+  if ((now - lastPidCalc) / PID_CALC_INTERVAL >= 1) {
+    doCalcPid();
+    lastPidCalc = millis();
+  }
+}
