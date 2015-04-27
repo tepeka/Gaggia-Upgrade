e2e6df5ed8aca2debd66b78f625901d8fe354b39
admin <admin@26dd76bd-a928-9f49-9f85-a93842d1d30d>
2015-04-27 21:39:18 +0000

    first sketch draft

---
+++ gaggia_do.ino
@@ -0,0 +1,82 @@
+#include <PID_v1.h>
+
+
+const int INVALID_TEMP = -274; // °C
+const int TEMP_MEM_SIZE = 10;
+
+bool led_on = true;
+bool led_pulse = true;
+
+int tempMem[TEMP_MEM_SIZE];
+int tempMemIdx = 0;
+int tempMemSum = 0;
+int tempMemAvg = 0;
+
+double Setpoint, Input, Output;
+PID myPID(&Input, &Output, &Setpoint, 200, 50, 0, DIRECT);
+int WindowSize = 2000;
+unsigned long windowStartTime;
+
+void doInit() {
+  windowStartTime = millis();
+  Setpoint = 90;
+  myPID.SetOutputLimits(0, WindowSize);
+  myPID.SetSampleTime(100);
+  myPID.SetMode(AUTOMATIC);
+  tempMem[0] = INVALID_TEMP;
+}
+
+void doLedHandling() {
+  if (!led_on) {
+    ledOff();
+
+  } else if (led_pulse) {
+    ledPulseStep();
+
+  } else {
+    ledOn();
+  }
+}
+
+void doReadTemp() {
+  // -- calc avg temp
+  int temp = rtdReadTemp();
+  if (tempMem[0] == INVALID_TEMP) {
+    // initialize
+    for (int i = 0; i < TEMP_MEM_SIZE; i++) {
+      tempMem[i] = tempMemAvg = temp;
+      tempMemSum += temp;
+    }
+  } else {
+    tempMemSum -= tempMem[tempMemIdx];
+    tempMem[tempMemIdx] = temp;
+    tempMemSum += tempMem[tempMemIdx];
+    tempMemIdx = (tempMemIdx + 1) % TEMP_MEM_SIZE;
+    tempMemAvg = tempMemSum / TEMP_MEM_SIZE;
+  }
+  // -- set led state depending on temp
+  if (tempMemAvg < Setpoint - 2) led_pulse = true;
+  else if (tempMemAvg > Setpoint + 2) led_pulse = true;
+  else led_pulse = false;
+  led_on = true;
+}
+
+void doCalcPid() {
+  Input = tempMemAvg;
+  myPID.Compute();
+  if (millis() - windowStartTime > WindowSize)
+  {
+    windowStartTime += WindowSize;
+  }
+  Serial.print(tempMemAvg);
+  Serial.print("deg C, relay: ");
+  if (Output < millis() - windowStartTime) {
+    digitalWrite(RELAY_PWM_PIN, HIGH);
+    Serial.println(HIGH);
+  } else {
+    digitalWrite(RELAY_PWM_PIN, LOW);
+    Serial.println(LOW);
+  }
+}
+
+
---
+++ led.ino
@@ -0,0 +1,38 @@
+int val = 0;
+int pin;
+boolean up = true;
+
+void ledInit(int p) {
+  pin = p;
+  pinMode(pin, OUTPUT);
+}
+
+void ledPulseStep() {
+  if (up) {
+    val = val + 10;
+  } else {
+    val = val - 10;
+  }
+  if (val > 255) {
+    val = 255;
+    up = false;
+  }
+  if (val < 0) {
+    val = 0;
+    up = true;
+  }
+
+  analogWrite(pin, val);
+  //if(val > 0)
+  //  delay(20);
+  //else
+  //  delay(200);
+}
+
+void ledOn() {
+  analogWrite(pin, 255);
+}
+
+void ledOff() {
+  analogWrite(pin, 0);
+}
