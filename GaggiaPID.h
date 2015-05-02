93f7812988e537336696eaee00760a40ca8e1625
admin <admin@26dd76bd-a928-9f49-9f85-a93842d1d30d>
2015-05-02 12:18:04 +0000

    encapsulate pid

---
+++ GaggiaPID.h
@@ -0,0 +1,31 @@
+#ifndef GaggiaPID_h
+#define GaggiaPID_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <PID_v1.h>
+
+class GaggiaPID {
+  public:
+    GaggiaPID(int setpoint, int p, int i, int d, int windowSize);
+    ~GaggiaPID();
+    bool Calculate(int temperature);
+    
+  private:
+    PID* m_pid;
+    double m_setpoint;
+    double m_input;
+    double m_output;
+    int m_p;
+    int m_i;
+    int m_d;
+    int m_windowSize;
+    unsigned long m_windowStartTime;
+    
+};
+
+#endif
