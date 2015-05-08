ccc8a34c55bf07cd70361da7d6331c17407fa73a
admin <admin@26dd76bd-a928-9f49-9f85-a93842d1d30d>
2015-05-08 22:50:27 +0000

    code improvements add poti

--- projects/Gaggia Upgrade/arduino sketch/GaggiaPID.cpp
+++ projects/Gaggia Upgrade/arduino sketch/GaggiaPID.cpp
@@ -12,13 +12,21 @@ GaggiaPID::GaggiaPID(int setpoint, int p, int i, int d, int windowSize) {
   m_pid->SetOutputLimits(0, m_windowSize);
   m_pid->SetSampleTime(100);
   m_pid->SetMode(AUTOMATIC);
+  m_pid->SetControllerDirection(REVERSE);
   m_windowStartTime = millis();
 }
 
-GaggiaPID::~GaggiaPID() { 
+GaggiaPID::~GaggiaPID() {
   delete m_pid;
 }
 
+void GaggiaPID::UpdateSetpoint(int setpoint) {
+  m_setpoint = setpoint;
+}
+
+int GaggiaPID::GetSetpoint() {
+  return m_setpoint;
+}
 
 bool GaggiaPID::Calculate(int temperature) {
   m_input = temperature;
@@ -31,5 +39,5 @@ bool GaggiaPID::Calculate(int temperature) {
     return true;
   } else {
     return false;
-  }  
+  }
 }
--- projects/Gaggia Upgrade/arduino sketch/gaggia.ino
+++ projects/Gaggia Upgrade/arduino sketch/gaggia.ino
@@ -1,17 +1,9 @@
-#include <PlayingWithFusion_MAX31865.h>
-#include <SPI.h>
-#include <PID_v1.h>
-
-#include "Light.h"
-#include "RTD.h"
-#include "GaggiaPID.h"
-
 const int LED_PWM_PIN = 13;
 const int RELAY_PWM_PIN = 12;
 const int RTD_PWM_PIN = 9;
 
 const int LED_HANDLING_INTERVAL = 20;
-const int TEMP_READ_INTERVAL = 100;
+const int TEMP_READ_INTERVAL = 50;
 const int PID_CALC_INTERVAL = 1;
 
 unsigned long lastLedHandling = 0;
