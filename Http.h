239a3ab6afe2857c35140910a29b67034a7c3905
admin <admin@26dd76bd-a928-9f49-9f85-a93842d1d30d>
2015-05-11 22:05:56 +0000

    add http (buggy!)

---
+++ Http.h
@@ -0,0 +1,19 @@
+#ifndef Http_h
+#define Http_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class Http {
+  public:
+    Http();
+    ~Http();
+    void Get(String url, String params[]);
+    
+  private:
+};
+
+#endif
--- gaggia.ino
+++ gaggia.ino
@@ -1,14 +1,17 @@
 const int LED_PWM_PIN = 13;
-const int RELAY_PWM_PIN = 12;
+const int RELAY_DIG_PIN = 12;
 const int RTD_PWM_PIN = 9;
+const int POTI_ANA_PIN = A0;
 
 const int LED_HANDLING_INTERVAL = 20;
 const int TEMP_READ_INTERVAL = 50;
 const int PID_CALC_INTERVAL = 1;
+const int SEND_HTTP_INTERVAL = 2000;
 
 unsigned long lastLedHandling = 0;
 unsigned long lastTempRead = 0;
 unsigned long lastPidCalc = 0;
+unsigned long lastSendHttp = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -31,5 +34,10 @@ void loop() {
   if ((now - lastPidCalc) / PID_CALC_INTERVAL >= 1) {
     doCalcPid();
     lastPidCalc = millis();
+  }
+  // -- execute send http
+  if ((now - lastSendHttp) / SEND_HTTP_INTERVAL >= 1) {
+    doSendHttp();
+    lastSendHttp = millis();
   }
 }
