a60e4cca754deee5ee39b655558f085942b53daa
admin <admin@26dd76bd-a928-9f49-9f85-a93842d1d30d>
2015-05-18 20:16:03 +0000

    http

---
+++ projects/Gaggia Upgrade/arduino sketch/HttpWrapper.h
@@ -0,0 +1,22 @@
+#ifndef HttpWrapper_h
+#define HttpWrapper_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+class HttpWrapper {
+  public:
+    HttpWrapper();
+    ~HttpWrapper();
+    void Get(char server[], String path);
+    
+  private:
+};
+
+#endif
